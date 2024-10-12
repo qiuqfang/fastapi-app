8eeccc3c9be32946cd534c892933a3a416972aec
pdx <qiuqfang98@qq.com>
2024-10-12 16:53:46 +0800

    fix: config main.py

--- config/main.py
+++ config/main.py
@@ -1,9 +1,11 @@
+from typing import Optional
+
 from pydantic import BaseSettings
 import os
 
 
 class Settings(BaseSettings):
-    MODE = os.getenv('MODE')
+    MODE: Optional[str] = os.getenv('MODE')
     MYSQL_HOST = "mysql" if MODE == 'PROD' else "127.0.0.1"
     REDIS_HOST = "redis" if MODE == 'PROD' else "127.0.0.1"
     SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://root:123456@{MYSQL_HOST}:3306/fastapi_db"
